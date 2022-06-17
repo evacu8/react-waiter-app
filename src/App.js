fff8b1bc40fc3da8e075b3ea747cf75c4e43c36d
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-17 13:23:42 +0200

    Connect db.json with redux store

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Table from './components/pages/Table/Table';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { fetchTables } from './redux/tablesRedux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => fetchTables(dispatch), [dispatch]);
+
   return (
     <Container>
        <Header />
--- src/components/features/AllTables/AllTables.js
+++ src/components/features/AllTables/AllTables.js
@@ -1,8 +1,23 @@
+import { useSelector } from "react-redux";
+import { getAllTables } from "../../../redux/tablesRedux";
+import { Container } from "react-bootstrap";
+import TableCard from "../TableCard/TableCard"
+
 const AllTables = () => {
+
+  const tables = useSelector(state => getAllTables(state));
+
   return (
-    <div>
-      <h1>AllTables</h1>
-    </div>
+    <Container>
+      <h2>AllTables</h2>
+      <ul className="d-flex flex-wrap row">
+        {tables.map(table => 
+          <TableCard 
+            key={table.id} 
+          />)}
+      </ul>
+
+    </Container>
   );
 };
 
